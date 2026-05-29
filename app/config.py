20b01ddd9c17285f2ca1afbfb5c120ab86ea7410
giotosi94 <gio_tosi@hotmail.it>
2026-05-29 14:53:30 +0200

    Update config.py

--- app/config.py
+++ app/config.py
@@ -3,7 +3,7 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     MONGODB_URL: str = "mongodb://localhost:27017"
     DB_NAME: str = "swinglab"
-    FMP_API_KEY: str = ""
+    TWELVEDATA_API_KEY: str = ""
 
     class Config:
         env_file = ".env"
