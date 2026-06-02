9dd47faf2c8db8dfe5d4bb8a2429a6901089ce26
giotosi94 <gio_tosi@hotmail.it>
2026-06-02 20:28:36 +0200

    Update config.py

--- app/config.py
+++ app/config.py
@@ -4,6 +4,8 @@ class Settings(BaseSettings):
     MONGODB_URL: str = "mongodb://localhost:27017"
     DB_NAME: str = "swinglab"
     TWELVEDATA_API_KEY: str = ""
+    TELEGRAM_BOT_TOKEN: str = ""
+    TELEGRAM_CHAT_ID: str = ""
 
     class Config:
         env_file = ".env"
