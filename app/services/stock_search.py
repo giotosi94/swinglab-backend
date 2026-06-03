02ebef0d1943fd22f29ef5189383bab5c0faa2ff
giotosi94 <gio_tosi@hotmail.it>
2026-06-03 21:05:55 +0200

    Update stock_search.py

--- app/services/stock_search.py
+++ app/services/stock_search.py
@@ -24,7 +24,7 @@ async def search_and_analyze_stock(ticker):
         params = {
             "symbol": ticker,
             "interval": "1day",
-            "outputsize": 70,
+            "outputsize": 252,
             "apikey": settings.TWELVEDATA_API_KEY,
         }
         try:
